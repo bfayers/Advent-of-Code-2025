2cbfe346081625a2869ebdf5000b731f3ca2c2c5
bfayers <bfayers@users.noreply.github.com>
2025-12-01 11:51:02 +0000

    Use modulo, much tidier

--- day1/main.go
+++ day1/main.go
@@ -15,6 +15,10 @@ type Instruction struct {
 	Amount    int
 }
 
+func calculate_new_position(position int, move int) int {
+	return (position + move + 100) % 100
+}
+
 func part1(instructions []Instruction) int {
 	// The dial has a lower bound of 0 and an upper bound of 99
 	// Going below 0 wraps to 99, and above 99 wraps to 0
@@ -22,32 +26,11 @@ func part1(instructions []Instruction) int {
 	// In part 1 we want to know how many times an instruction causes the dial to rest at 0
 	position := 50
 	zero_count := 0
-	// fmt.Printf("The dial is at: %d\n", position)
 	for _, instruction := range instructions {
-		if instruction.Direction == "L" {
-			position = position - instruction.Amount
-			// fmt.Printf("The dial has rotated left by %d and is now at %d\n", instruction.Amount, position)
-		}
-		if instruction.Direction == "R" {
-			position = position + instruction.Amount
-			// fmt.Printf("The dial has rotated right by %d and is now at %d\n", instruction.Amount, position)
-		}
-		// Handle wrapping around
-		// Keep adjusting position until it is within bounds
-		for position < 0 || position > 99 {
-			if position < 0 {
-				position = 100 - (position * -1)
-				// fmt.Printf("The dial wrapped around to %d\n", position)
-			}
-			if position > 99 {
-				position = 0 + (position - 100)
-				// fmt.Printf("The dial wrapped around to %d\n", position)
-			}
-		}
+		position = calculate_new_position(position, instruction.Amount)
 		// Check if we are at 0
 		if position == 0 {
 			zero_count++
-			// fmt.Printf("The dial is at 0! Total zero count: %d\n", zero_count)
 		}
 	}
 	return zero_count
@@ -62,31 +45,19 @@ func part2(instructions []Instruction) int {
 	zero_count := 0
 	// fmt.Printf("The dial is at: %d\n", position)
 	for _, instruction := range instructions {
+		if instruction.Amount < 0 {
+			instruction.Amount = instruction.Amount * -1
+		}
 		for i := 0; i < instruction.Amount; i++ {
 			if instruction.Direction == "L" {
-				position = position - 1
-				// fmt.Printf("The dial has rotated left by %d and is now at %d\n", instruction.Amount, position)
+				position = calculate_new_position(position, -1)
 			}
 			if instruction.Direction == "R" {
-				position = position + 1
-				// fmt.Printf("The dial has rotated right by %d and is now at %d\n", instruction.Amount, position)
-			}
-			// Handle wrapping around
-			// Keep adjusting position until it is within bounds
-			for position < 0 || position > 99 {
-				if position < 0 {
-					position = 100 - (position * -1)
-					// fmt.Printf("The dial wrapped around to %d\n", position)
-				}
-				if position > 99 {
-					position = 0 + (position - 100)
-					// fmt.Printf("The dial wrapped around to %d\n", position)
-				}
+				position = calculate_new_position(position, 1)
 			}
 			// Check if we are at 0
 			if position == 0 {
 				zero_count++
-				// fmt.Printf("The dial is at 0! Total zero count: %d\n", zero_count)
 			}
 		}
 	}
@@ -110,7 +81,11 @@ func main() {
 			continue
 		}
 		direction := letter_re.FindString(this_line)
-		instructions = append(instructions, Instruction{Direction: direction, Amount: number})
+		if direction == "L" {
+			instructions = append(instructions, Instruction{Direction: "L", Amount: -number})
+		} else {
+			instructions = append(instructions, Instruction{Direction: "R", Amount: number})
+		}
 	}
 
 	// Part 1

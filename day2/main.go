a5b80e2916e1c667ecbb2bcd054e92dbe69a9f6a
Ben Fayers <bfayers@users.noreply.github.com>
2025-12-02 11:32:58 +0000

    Solve for day2, tidied up a bit

---
+++ day2/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type id_range struct {
+	min int
+	max int
+}
+
+func check_repeated(s string, max_repeats int) bool {
+	for j := 1; j <= len(s)/2; j++ {
+		first_part := s[:j]
+		repeat_count := strings.Count(s, first_part)
+
+		if repeat_count*len(first_part) == len(s) && repeat_count <= max_repeats {
+			return true
+		}
+	}
+	return false
+}
+
+func part1(id_ranges []id_range) int {
+	// We need to identify repeating patterns of numbers, they could be as little 1 digit twice (eg: 55 = 5 twice) but no upper bound on that behaviour
+	// The 'invalid' ids are ONLY made of the repeating numbers TWICE (eg: 824824 is invalid but 824824824 is not)
+	var total_invalid int
+	for _, id_range := range id_ranges {
+		for i := id_range.min; i <= id_range.max; i++ {
+			// Get the string representation of the number
+			num_str := strconv.Itoa(i)
+			if check_repeated(num_str, 2) {
+				total_invalid += i
+			}
+		}
+	}
+	return total_invalid
+}
+
+func part2(id_ranges []id_range) int {
+	// Same as part 1, identifying repeating patterns, but now the 'invalid' ids are repeated _AT LEAST_ twice (eg: 824824 is invalid, as is 824824824)
+	var total_invalid int
+	for _, id_range := range id_ranges {
+		// fmt.Printf("Searching through: %d-%d\n", id_range.min, id_range.max)
+		// Get the string representation of the number
+		for i := id_range.min; i <= id_range.max; i++ {
+			num_str := strconv.Itoa(i)
+			// possible digits for 'repeating' can only be up to half the length of the string,
+			// find all possible repeating sequences and check if the string is made entirely of that
+			if check_repeated(num_str, len(num_str)) {
+				total_invalid += i
+			}
+		}
+	}
+	return total_invalid
+}
+
+func main() {
+	// Load the data
+	dat, _ := os.Open("input.txt")
+	// Scan over the input to make an array of lines
+	scanner := bufio.NewScanner(dat)
+	var id_ranges []id_range
+	for scanner.Scan() {
+		this_line := scanner.Text()
+		ranges := strings.Split(this_line, ",")
+		for _, range_str := range ranges {
+			range_values := strings.Split(range_str, "-")
+			min, _ := strconv.Atoi(range_values[0])
+			max, _ := strconv.Atoi(range_values[1])
+			id_ranges = append(id_ranges, id_range{min: min, max: max})
+		}
+
+	}
+
+	fmt.Printf("Part 1: %d\n", part1(id_ranges))
+	fmt.Printf("Part 2: %d\n", part2(id_ranges))
+}
